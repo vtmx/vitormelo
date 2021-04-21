ef049b5ffc334b49a4a7285dcbe213fb02a23f85
vtmx <vtmx22@gmail.com>
2021-04-21 17:31:47 -0300

    custom eleventy

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/_data');
+  /*   eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/js'); */
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+    },
+    passthroughFileCopy: true,
+  };
+};
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Vitor Melo
+
+```sh
+# install packages
+yarn install
+
+# start project
+yarn dev
+
+# build project
+yarn build
+```
