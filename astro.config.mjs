e3fa8d87bfa2b09e75b8dc7ec330b4aefce4940d
Vitor Melo <vtmx22@gmail.com>
2022-07-23 01:11:00 -0300

    add netlify function

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import vue from '@astrojs/vue';
+import netlify from '@astrojs/netlify/functions';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
+  adapter: netlify(),
 });
