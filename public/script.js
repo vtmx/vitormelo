1fff2a09cbbb032e03dbaaec2e84814c011bcddf
vtmx <vtmx22@gmail.com>
2026-01-28 21:53:04 -0300

    Update main path css and js

---
+++ public/script.js
@@ -0,0 +1,5 @@
+import colorControl from './js/color-control.js';
+import sectionControl from './js/section-control.js';
+
+colorControl.init();
+sectionControl.init();
--- static/js/script.js
+++
@@ -1,5 +0,0 @@
-import colorControl from './color-control.js';
-import sectionControl from './section-control.js';
-
-colorControl.init();
-sectionControl.init();
